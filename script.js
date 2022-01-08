c9a8965f38bbf3862ff81cf9c535a5bf4e4b206b
Charles Pinheiro <charles_fagundes@hotmail.com>
2022-01-08 14:03:30 -0300

    feat: restart game function

--- script.js
+++ script.js
@@ -16,10 +16,16 @@ const map = [
     "WWWWWWWWWWWWWWWWWWWWW",
 ];
 
+let boxWin = document.createElement("div");
+let message = document.createElement("p");
+let restartButton = document.createElement("button");
+
 let container = document.getElementById("container");
 
 let player = document.createElement("div");
 player.id = "p1";
+
+let start = document.createElement("div");
 
 const maze = (map) => {
 
@@ -39,7 +45,6 @@ const maze = (map) => {
             }
 
             if (map[i][j] === "S") {
-                let start = document.createElement("div");
                 start.classList.add("start");
                 start.appendChild(player);
                 line.appendChild(start);
@@ -63,4 +68,66 @@ const maze = (map) => {
 
 maze(map);
 
+const winner = () => {
+    container.appendChild(boxWin);
+
+    message.id = "winMessage";
+    message.innerHTML = "Você Venceu!"
+    
+    restartButton.id = "winButton";
+    restartButton.innerHTML = "Recomeçar"
+    
+    boxWin.appendChild(message);
+    boxWin.appendChild(restartButton);
+}
+
+const restartGame = () => {
+    start.appendChild(player);
+    container.removeChild(boxWin);
+}
+
 let counter = 0;
+
+document.addEventListener("keydown", (event) => {
+    const keyName = event.key;
+
+    let current = p1.parentElement;
+    let next = p1.parentElement.nextSibling;
+    let previous = p1.parentElement.previousSibling;
+    let down = current.parentElement.nextSibling;
+    let up = current.parentElement.previousSibling;
+
+    if (keyName === "ArrowDown" && down.childNodes[counter].classList.contains("way")) {
+        down.childNodes[counter].appendChild(player);
+    }
+
+    if (keyName === "ArrowUp" && up.childNodes[counter].classList.contains("way")) {
+        up.childNodes[counter].appendChild(player);
+    }
+
+    if (keyName === "ArrowRight") {
+
+        if (next.classList[0] === "way") {
+            next.appendChild(player);
+            counter++;
+        }
+
+        if (next.classList[0] === "finish") {
+            next.appendChild(player);
+            winner();
+            boxWin.classList.add("win");
+            // winMessage[0].classList.remove('t');
+            // winMessage[0].classList.add('win');
+        }
+    }
+
+    if (keyName === "ArrowLeft") {
+
+        if (previous.classList[0] === "way") {
+            previous.appendChild(player);
+            counter--;
+        }
+    }
+});
+
+restartButton.addEventListener("click", restartGame);
